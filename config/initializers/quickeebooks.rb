fd63ad41f95ba3cd9626e32ee20d303e11275765
Christian <zarski@gmail.com>
2013-10-29 17:33:06 -0400

    Load from yml file and not ENV

--- config/initializers/quickeebooks.rb
+++ config/initializers/quickeebooks.rb
@@ -1,5 +1,6 @@
+QB_CREDS = YAML.load(File.read(File.expand_path('./config/quickbooks.yml', Rails.root)))
 
-$qb_oauth_consumer = OAuth::Consumer.new(ENV['QB_KEY'], ENV['QB_SECRET'], {
+$qb_oauth_consumer = OAuth::Consumer.new(QB_CREDS['key'], QB_CREDS['secret'], {
   site:                "https://oauth.intuit.com",
   request_token_path:  "/oauth/v1/get_request_token",
   authorize_url:       "https://appcenter.intuit.com/Connect/Begin",
