32428149f05b961546ae4bcb858921d9d5cc0f17
Christian <zarski@gmail.com>
2013-10-29 17:36:14 -0400

    Special QBO actions for getting properly auth'd

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -61,6 +61,24 @@ class VendorsController < ApplicationController
     end
   end
 
+  def authenticate
+    callback = oauth_callback_vendors_url
+    token = $qb_oauth_consumer.get_request_token(:oauth_callback => callback)
+    session[:qb_request_token] = token
+    redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
+  end
+
+  def oauth_callback
+    at = session[:qb_request_token].get_access_token(:oauth_verifier => params[:oauth_verifier])
+    session[:token] = at.token
+    session[:secret] = at.secret
+    session[:realm_id] = params['realmId']
+    flash.notice = "Your QuickBooks account has been successfully linked."
+    @msg = 'Redirecting. Please wait.'
+    @url = vendors_path
+    render 'close_and_redirect', layout: false
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vendor
@@ -71,4 +89,5 @@ class VendorsController < ApplicationController
     def vendor_params
       params.require(:vendor).permit(:name)
     end
+
 end
