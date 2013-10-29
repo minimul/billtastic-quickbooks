95ffeace1efcf4e7f271a97028da3e9df3ad3390
Christian <zarski@gmail.com>
2013-10-29 16:00:11 -0400

    Add Vendor scaffolding

---
+++ app/controllers/vendors_controller.rb
@@ -0,0 +1,74 @@
+class VendorsController < ApplicationController
+  before_action :set_vendor, only: [:show, :edit, :update, :destroy]
+
+  # GET /vendors
+  # GET /vendors.json
+  def index
+    @vendors = Vendor.all
+  end
+
+  # GET /vendors/1
+  # GET /vendors/1.json
+  def show
+  end
+
+  # GET /vendors/new
+  def new
+    @vendor = Vendor.new
+  end
+
+  # GET /vendors/1/edit
+  def edit
+  end
+
+  # POST /vendors
+  # POST /vendors.json
+  def create
+    @vendor = Vendor.new(vendor_params)
+
+    respond_to do |format|
+      if @vendor.save
+        format.html { redirect_to @vendor, notice: 'Vendor was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @vendor }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @vendor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /vendors/1
+  # PATCH/PUT /vendors/1.json
+  def update
+    respond_to do |format|
+      if @vendor.update(vendor_params)
+        format.html { redirect_to @vendor, notice: 'Vendor was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @vendor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /vendors/1
+  # DELETE /vendors/1.json
+  def destroy
+    @vendor.destroy
+    respond_to do |format|
+      format.html { redirect_to vendors_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_vendor
+      @vendor = Vendor.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def vendor_params
+      params.require(:vendor).permit(:name)
+    end
+end
